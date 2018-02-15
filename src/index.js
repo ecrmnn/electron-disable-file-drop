32fadd42b569714686c6646787f37d91246b6e45
Dave Jeffery <dave@davejeffery.com>
2018-02-15 15:27:17 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-'use strict';
-
-document.addEventListener('dragover', function (event) {
-  event.preventDefault();
-  return false;
-}, false);
-
-document.addEventListener('drop', function (event) {
-  event.preventDefault();
-  return false;
-}, false);
+document.addEventListener('dragover', event => event.preventDefault());
+document.addEventListener('drop', event => event.preventDefault());
